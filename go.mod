f50299cea4466aad6f3806238afe8b277a8a0b03
Luiz G. <luiz007@live.com>
2024-09-23 18:28:16 -0300

    Add automatic update using timer

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module qbit-tea
 
 go 1.23.1
 
-require github.com/charmbracelet/bubbletea v1.1.1
+require (
+	github.com/charmbracelet/bubbles v0.20.0
+	github.com/charmbracelet/bubbletea v1.1.1
+)
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
@@ -13,7 +16,7 @@ require (
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 	"os"
 	"qbit-tea/input"
 	"strings"
+	"time"
 
+	"github.com/charmbracelet/bubbles/timer"
 	tea "github.com/charmbracelet/bubbletea"
 )
+
+var timeout = time.Second * 5.0
 
 func checkError(err error) {
 	if err != nil {
@@ -16,13 +20,14 @@ func checkError(err error) {
 }
 
 type model struct {
-	Entries []input.Entry
-	cursor  int
-	addMode bool
+	UpdateTimer timer.Model
+	Entries     []input.Entry
+	cursor      int
+	addMode     bool
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return m.UpdateTimer.Init()
 }
 
 type actionMsg struct {
@@ -32,10 +37,17 @@ type actionMsg struct {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
+	case timer.TickMsg:
+		var cmd tea.Cmd
+		m.UpdateTimer, cmd = m.UpdateTimer.Update(msg)
+		return m, cmd
+
+	case timer.TimeoutMsg:
+        m.UpdateTimer.Timeout = timeout
+		m.UpdateTimer.Init()
+		return m, input.CmdUpdate
+
 	case input.MsgUpdate:
-		// m.Entries[m.cursor].Filename += msg.Output
-		// output, err := transmission.TransmissionList()
-		// checkError(err)
 		m.Entries = msg.Entries
 		return m, nil
 
@@ -64,6 +76,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m model) View() string {
 	var output strings.Builder
+	timeoutSec := fmt.Sprintf("%d", (m.UpdateTimer.Timeout / 1_000_000_000)+1)
+	output.WriteString(fmt.Sprintf("Updating in %s...\n", timeoutSec))
 	for index, entry := range m.Entries {
 		if index == m.cursor {
 			output.WriteRune('>')
@@ -89,7 +103,9 @@ func main() {
 		defer f.Close()
 	}
 
-	if _, err := tea.NewProgram(model{}).Run(); err != nil {
+	if _, err := tea.NewProgram(model{
+        UpdateTimer: timer.NewWithInterval(timeout, time.Millisecond),
+    }).Run(); err != nil {
 		fmt.Printf("Uh oh, there was an error: %v\n", err)
 		os.Exit(1)
 	}
